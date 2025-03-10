30a2562da3e3e2f049cdea80266d65225a77012f
Shuanglei Tao <tsl0922@gmail.com>
2025-03-10 10:34:32 +0800

    update calendar ui

--- GUI/Calendar.c
+++ GUI/Calendar.c
@@ -10,47 +10,74 @@
 static void DrawDateHeader(Adafruit_GFX *gfx, int16_t x, int16_t y, tm_t *tm, struct Lunar_Date *Lunar)
 {
     GFX_setCursor(gfx, x, y);
-    GFX_setFont(gfx, u8g2_font_wqy12b_t_lunar);
+    GFX_setTextColor(gfx, GFX_RED, GFX_WHITE);
+    GFX_setFont(gfx, u8g2_font_helvB18_tn);
+    GFX_printf(gfx, "%d", tm->tm_year + YEAR0);
     GFX_setTextColor(gfx, GFX_BLACK, GFX_WHITE);
-    GFX_printf(gfx, "%d年%02d月%02d日 星期%s", tm->tm_year + YEAR0, tm->tm_mon + 1, tm->tm_mday, Lunar_DayString[tm->tm_wday]);
+    GFX_setFont(gfx, u8g2_font_wqy12_t_lunar);
+    GFX_printf(gfx, "年");
+    GFX_setTextColor(gfx, GFX_RED, GFX_WHITE);
+    GFX_setFont(gfx, u8g2_font_helvB18_tn);
+    GFX_printf(gfx, "%02d", tm->tm_mon + 1);
+    GFX_setTextColor(gfx, GFX_BLACK, GFX_WHITE);
+    GFX_setFont(gfx, u8g2_font_wqy12_t_lunar);
+    GFX_printf(gfx, "月");
+    GFX_setTextColor(gfx, GFX_RED, GFX_WHITE);
+    GFX_setFont(gfx, u8g2_font_helvB18_tn);
+    GFX_printf(gfx, "%02d", tm->tm_mday);
+    GFX_setTextColor(gfx, GFX_BLACK, GFX_WHITE);
+    GFX_setFont(gfx, u8g2_font_wqy12_t_lunar);
+    GFX_printf(gfx, "日 ");
+    GFX_setFont(gfx, u8g2_font_wqy9_t_lunar);
+    GFX_printf(gfx, "星期%s", Lunar_DayString[tm->tm_wday]);
 
     LUNAR_SolarToLunar(Lunar, tm->tm_year + YEAR0, tm->tm_mon + 1, tm->tm_mday);
     GFX_setFont(gfx, u8g2_font_wqy9_t_lunar);
-    GFX_setCursor(gfx, x + 226, y);
-    GFX_printf(gfx, "农历: %s%s%s %s%s[%s]年", Lunar_MonthLeapString[Lunar->IsLeap], Lunar_MonthString[Lunar->Month],
+    GFX_setCursor(gfx, x + 270, y);
+    GFX_printf(gfx, "%s%s%s %s%s", Lunar_MonthLeapString[Lunar->IsLeap], Lunar_MonthString[Lunar->Month],
                      Lunar_DateString[Lunar->Date], Lunar_StemStrig[LUNAR_GetStem(Lunar)],
-                     Lunar_BranchStrig[LUNAR_GetBranch(Lunar)], Lunar_ZodiacString[LUNAR_GetZodiac(Lunar)]);
+                     Lunar_BranchStrig[LUNAR_GetBranch(Lunar)]);
+    GFX_setTextColor(gfx, GFX_RED, GFX_WHITE);
+    GFX_printf(gfx, "%s", Lunar_ZodiacString[LUNAR_GetZodiac(Lunar)]);
+    GFX_setTextColor(gfx, GFX_BLACK, GFX_WHITE);
+    GFX_printf(gfx, "年");
 }
 
 static void DrawWeekHeader(Adafruit_GFX *gfx, int16_t x, int16_t y)
 {
-    GFX_setTextColor(gfx, GFX_WHITE, GFX_RED);
-    GFX_fillRect(gfx, x, y, 380, 18, GFX_RED);
+    GFX_fillRect(gfx, x, y, 380, 24, GFX_RED);
+    GFX_fillRect(gfx, x + 50, y, 280, 24, GFX_BLACK);
+    GFX_setFont(gfx, u8g2_font_wqy9_t_lunar);
     for (int i = 0; i < 7; i++)
     {
-        GFX_setCursor(gfx, x + 15 + i * 55, y + 14);
+        if (i > 0 && i < 6)
+            GFX_setTextColor(gfx, GFX_WHITE, GFX_BLACK);
+        else
+            GFX_setTextColor(gfx, GFX_WHITE, GFX_RED);
+        GFX_setCursor(gfx, x + 15 + i * 55, y + 16);
         GFX_printf(gfx, "%s", Lunar_DayString[i]);
     }
 }
 
-static void DrawMonthDay(Adafruit_GFX *gfx, int16_t x, int16_t y, tm_t *tm, struct Lunar_Date *Lunar, uint8_t day)
+static void DrawMonthDay(Adafruit_GFX *gfx, int16_t x, int16_t y, tm_t *tm,
+                         struct Lunar_Date *Lunar, uint8_t day, bool weekend)
 {
-    if (day == tm->tm_mday)
-    {
-        GFX_fillCircle(gfx, x + 10, y + 9, 20, GFX_RED);
+    if (day == tm->tm_mday) {
+        GFX_fillCircle(gfx, x + 10, y + 12, 20, GFX_RED);
         GFX_setTextColor(gfx, GFX_WHITE, GFX_RED);
-    }
-    else
-    {
-        GFX_setTextColor(gfx, GFX_BLACK, GFX_WHITE);
+    } else {
+        GFX_setTextColor(gfx, weekend ? GFX_RED : GFX_BLACK, GFX_WHITE);
     }
 
-    GFX_setFont(gfx, u8g2_font_wqy12b_t_lunar);
-    GFX_setCursor(gfx, x + 2, y + 4);
+    GFX_setFont(gfx, u8g2_font_helvB14_tn);
+    if (day < 10)
+        GFX_setCursor(gfx, x + 6, y + 10);
+    else
+        GFX_setCursor(gfx, x + 2, y + 10);
     GFX_printf(gfx, "%d", day);
 
     GFX_setFont(gfx, u8g2_font_wqy9_t_lunar);
-    GFX_setCursor(gfx, x, y + 20);
+    GFX_setCursor(gfx, x, y + 24);
     uint8_t JQdate;
     if (GetJieQi(tm->tm_year + YEAR0, tm->tm_mon + 1, day, &JQdate) && JQdate == day)
     {
@@ -98,9 +125,11 @@ void DrawCalendar(uint32_t timestamp)
         DrawWeekHeader(&gfx, 10, 26);
 
         for (uint8_t i = 0; i < monthMaxDays; i++) {
-            int16_t x = 22 + (firstDayWeek + i) % 7 * 55;
+            int16_t w = (firstDayWeek + i) % 7;
+            bool weekend = (w  == 0) || (w == 6);
+            int16_t x = 22 + w * 55;
             int16_t y = (monthDayRows > 5 ? 60 : 65) + (firstDayWeek + i) / 7 * (monthDayRows > 5 ? 42 : 50);
-            DrawMonthDay(&gfx, x, y, &tm, &Lunar, i + 1);
+            DrawMonthDay(&gfx, x, y, &tm, &Lunar, i + 1, weekend);
         }
     } while(GFX_nextPage(&gfx, driver->write_image));
 
